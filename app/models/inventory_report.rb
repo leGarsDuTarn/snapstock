d755a75b2aa318df2b50e23f9cf9ce8784ac0f5f
Benjamin Grassiano <grassiano.b@gmail.com>
2026-01-25 17:13:44 +0100

    Modification model User et inventory_report

--- app/models/inventory_report.rb
+++ app/models/inventory_report.rb
@@ -1,3 +1,7 @@
 class InventoryReport < ApplicationRecord
   belongs_to :store
+  belongs_to :user
+
+  has_many :inventory_items, dependent: :destroy
+  accepts_nested_attributes_for :inventory_items
 end
