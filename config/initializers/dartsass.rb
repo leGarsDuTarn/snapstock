074d847db1570dcf5302a4380470fe73b38e2b34
Benjamin Grassiano <grassiano.b@gmail.com>
2026-01-27 17:22:52 +0100

    Debugging scss

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,4 +1,10 @@
 # config/initializers/dartsass.rb
 Rails.application.config.dartsass.builds = {
-  "application.bootstrap.scss"  => "application.css"
+  "application.scss" => "application.css"
 }
+
+# Important : initialiser comme un tableau vide si nil
+Rails.application.config.dartsass.load_paths ||= []
+
+# Ajouter le chemin node_modules
+Rails.application.config.dartsass.load_paths << Rails.root.join("node_modules").to_s
