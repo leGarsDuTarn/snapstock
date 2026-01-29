e9ca72cda44faf8d665c5514b1e732dc794a0c06
Benjamin Grassiano <grassiano.b@gmail.com>
2026-01-29 13:44:49 +0100

    Debuging code 404 suite a controller stimulus non trouvee

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,5 @@
-// app/javascript/application.js
-
-// 1. Charge Turbo
 import "@hotwired/turbo-rails";
-
-// 2. Charge tes controlleurs Stimulus
-import "./controllers";
-
-// 3. Charge Popper (nécessaire pour dropdowns/tooltips)
+import "controllers/application";
 import "@popperjs/core";
-
-// 4. Charge Bootstrap ET l'assigne à window
 import * as bootstrap from "bootstrap";
 window.bootstrap = bootstrap;
