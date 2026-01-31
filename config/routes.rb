063d820f8766c22b0735bf0d1b8ff5805b37de55
Benjamin Grassiano <grassiano.b@gmail.com>
2026-01-31 15:43:13 +0100

    Mise a jour du rooting pour les strates

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,29 @@ Rails.application.routes.draw do
   resource :session, only: %i[new create destroy]
   resources :passwords, param: :token, only: %i[new create edit update]
 
-  # --- 2. DASHBOARD & RACINE ---
-  # La page d'accueil est le tableau de bord
+  # --- 2. DASHBOARD ---
   root "dashboards#show"
   resource :dashboard, only: [ :show ]
 
-  # --- 3. MÉTIER ---
+  # --- 3. MÉTIER & TERRAIN ---
   resources :stores do
-    # shallow: true ->
-    # - Pour créer : /stores/1/inventory_reports/new (Besoin de l'ID du magasin)
-    # - Pour voir/modifier : /inventory_reports/5 (L'ID du rapport suffit, l'URL est courte)
     resources :inventory_reports, shallow: true
     resources :employees, shallow: true
     resources :managers, shallow: true
   end
 
-  # --- 4. DONNÉES DE RÉFÉRENCE ---
+  # --- 4. CONFIGURATION ENSEIGNES ---
+  # Les strates et l'assortiment sont rattachés directement à la marque.
+  resources :brands do
+    resources :strata, shallow: true
+    resources :assortments, shallow: true
+  end
+
+  # --- 5. RÉFÉRENTIEL GLOBAL ---
   resources :products
-  resources :brands
   resources :categories
 
-  # --- 5. PWA (Progressive Web App) ---
+  # --- 6. PWA ---
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
